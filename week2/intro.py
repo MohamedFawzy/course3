0690fc32ef8e413d0aaccad9465ef750373fdef2
MohamedFawzy <mfawzy22@gmail.com>
2016-07-20 20:33:41 +0200

    add example in intro

--- week2/intro.py
+++ week2/intro.py
@@ -1,1 +1,11 @@
-print 'hello world'
+import re
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('From:', line):
+        print line
+    if re.search('^X.*:', line):
+        print line
+x = 'my 2 favorites number are 19 and 92'
+y = re.findall('[0-9]', x)
+print y
